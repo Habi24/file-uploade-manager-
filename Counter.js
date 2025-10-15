704b02053485238e54d0021e5a410848d45f54ad
Habi24 <habibunissa2005@gmail.com>
2025-10-15 20:33:45 +0530

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,25 @@
+/*import React, { useState, useEffect } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("saai");
+
+  useEffect(() => {
+    console.log(`Name changed to: ${name}`);
+  }, [name]);
+
+  useEffect(() => {
+    console.log(`Count changed to: ${count}`);
+  }, [count]);
+
+  return (
+    <div>
+      <h1> count {count} </h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <h1>MY NAME is {name}</h1>
+      <button onClick={() => setName("Sree")}>Change Name</button>
+    </div>
+  );
+}
+
+export default Counter;*/
---
+++ Greeting.jsx
@@ -0,0 +1,11 @@
+/*import React from "react";
+
+function Greeting({ name, age }) {
+  return (
+    <div>
+      <h3>Hello, my name is {name} and I am {age} years old.</h3>
+    </div>
+  );
+}
+
+export default Greeting;*/
